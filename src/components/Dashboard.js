8760c64ce6e84b275248e1e4869587a60bd16f35
lenaabdu <54982546+lenaabdu@users.noreply.github.com>
2020-10-28 20:21:20 +0000

    update Dasboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import ChatBot from "react-simple-chatbot";
 
-const Dashboard = () => {
-    return (
-        <div>
-            Dashboard  
-        </div>
-    )
+function Dashboard(props) {
+    const config = {
+        width: "300px",
+        height: "400px",
+        floating: true
+    };
+    const steps = [{
+            id: "Greet",
+            message: "Hello, Welcome",
+            trigger: "Ask Name"
+        },
+        {
+            id: "Ask Name",
+            message: "Please type your name?",
+            trigger: "Waiting user input for name"
+        },
+        {
+            id: "Waiting user input for name",
+            user: true,
+            trigger: "Asking welocome"
+        },
+        {
+            id: "Asking welocome",
+            message: "Hi {previousValue}, Glad to know you !!",
+            trigger: "Done"
+        },
+        {
+            id: "Done",
+            message: "Have a great day !!",
+            end: true
+        }
+    ];
+
+    return <ChatBot steps = { steps } {...config }
+    />;
 }
-
 export default Dashboard;

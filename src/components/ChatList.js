bc15671e210f68b9ebc0f510107bd8207d7df2e3
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-11-07 20:18:27 +0000

    add anxiety chat options

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -78,7 +78,8 @@ const ChatList = () => {
         {
             id: "struggleOptions",
             options: [
-                { value: 1, label: 'sleep', trigger: 'confirmSleep' }
+                { value: 1, label: 'Sleep', trigger: 'confirmSleep' },
+                { value: 2, label: 'Anxiety', trigger: 'confirmAnxiety' }
               ]
         },
         {
@@ -90,6 +91,18 @@ const ChatList = () => {
             id: "confirmSleepOptions",
             options: [
                 { value : 1, label: 'Yes', trigger: 'sleepSympathy' },
+                { value : 2, label: 'No', trigger: 'return' }
+              ]
+        },
+        {
+            id: "confirmAnxiety",
+            message: "It sounds like you're having trouble with anxiety. Is that right?",
+            trigger: "confirmAnxietyOptions"
+        },
+        {
+            id: "confirmAnxietyOptions",
+            options: [
+                { value : 1, label: 'Yes', trigger: 'anxietySympathy' },
                 { value : 2, label: 'No', trigger: 'return' }
               ]
         },
@@ -157,6 +170,58 @@ const ChatList = () => {
             message: `Would you like to tell me more about your problem with sleep? I won't have anything to say, but I am a very good listener.`,
             trigger: "talkMoreOptions"
         },
+
+
+        //start of anxiety section
+        {
+            id: "anxietySympathy",
+            message: "I'm sorry that you're having trouble with anxiety. Can you tell me what the problem is in more detail?",
+            trigger: "anxietyDetailOptions"
+        },
+        {
+            id: "anxietyDetailOptions",
+            options: [
+                { value: 1, label: `I get over-anxious about things that worry me`, trigger: 'veryAnxious' },
+                { value: 2, label: `I'm always anxious`, trigger: 'alwaysAnxious' },
+                { value: 3, label: `I have episodes where I'm so anxious I feel like I'll die`, trigger: 'panic' },
+              ]
+        },
+        {
+            id: "veryAnxious",
+            message: `It must be difficult to have such strong anxious feelings. Perhaps I can help.`,
+            trigger: "veryAnxiousAdvice"
+        },
+        {
+            id: "veryAnxiousAdvice",
+            message: `It's a good idea to seek medical advice for over-anxious feelings. In the meantime, it may help to do a breathing exercise for ten minutes when you find it difficult to cope.`,
+            trigger: "anxiousTalkMoreQuestion"
+        },
+        {
+            id: "alwaysAnxious",
+            message: `It must be frustrating that you're always feeling anxious. Perhaps I can help.`,
+            trigger: "alwaysAnxiousAdvice"
+        },
+        {
+            id: "alwaysAnxiousAdvice",
+            message: `Always feeling anxious might indicate an anxiety disorder. It's a good idea to seek medical advice. In the meantime, it may help to exercise for half an hour a day.`,
+            trigger: "anxiousTalkMoreQuestion"
+        },
+        {
+            id: "panic",
+            message: `It must be very difficult to cope with bouts of intense anxiety. Perhaps I can help.`,
+            trigger: "panicAdvice"
+        },
+        {
+            id: "panicAdvice",
+            message: `Episodes of intense anxiety may indicate a panic disorder. Although they are often scary, they do not indicate a physical problem. It's a good idea to seek medical advice. In the meantime, it may help to carry with you something you can use to distract yourself during an episode.`,
+            trigger: "anxiousTalkMoreQuestion"
+        },
+        {
+            id: "anxiousTalkMoreQuestion",
+            message: `Would you like to tell me more about your problem with anxiety? I won't have anything to say, but I am a very good listener.`,
+            trigger: "talkMoreOptions"
+        },
+        //end of anxiety section
         {
             id: "talkMoreOptions",
             options: [
@@ -183,6 +248,7 @@ const ChatList = () => {
         },
     ];
 
+    
     return (
         <div>
             <ChatBot steps={sleepSteps} {...config} />

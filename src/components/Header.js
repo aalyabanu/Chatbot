ccdcf84eb63c2017676ac9c12be7e056d99118de
Aalya Banu <aalya.banu@gmail.com>
2020-11-18 20:08:53 +0000

    Logo added

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "../assets/moodbotLOGO.svg"
+
+const Header = () => {
+
+
+    return (
+        <>
+           <header>
+           <img src={logo} alt="logo" />
+
+
+
+            </header>
+        </>
+    )
+}
+
+export default Header;

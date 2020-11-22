68c329332dc088886724feefb01657e0814dad5f
Aalya Banu <aalya.banu@gmail.com>
2020-11-22 09:12:49 +0000

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./output.css";
 import Home from "./components/Home"
-import Header from "./components/Header"
+import Header from "./components/Header";
+import About from "./components/About"
 
 
 const App = () => (
     <>
-<Header/>
+
     <div className="container">
+
         <Router>
+        <Header/>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/about" component={About} />
             </Switch>
 
         </Router>
